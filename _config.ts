24f24079afd9fa45c1cbab401ac06b4be6b75c38
luke-strange <lukestrange123@gmail.com>
2025-01-08 19:23:08 +0000

    Initial commit

---
+++ _config.ts
@@ -0,0 +1,26 @@
+import lume from "lume/mod.ts";
+import basePath from "lume/plugins/base_path.ts";
+import favicon from "lume/plugins/favicon.ts";
+import metas from "lume/plugins/metas.ts";
+import nav from "lume/plugins/nav.ts";
+import postcss from "lume/plugins/postcss.ts";
+import googleFonts from "lume/plugins/google_fonts.ts";
+
+
+const site = lume({
+    src: './src',
+    location: new URL("https://luke-strange.github.io/website"),
+});
+
+site.use(basePath());
+// site.use(favicon());
+site.use(metas());
+site.use(nav());
+site.use(postcss());
+
+site.use(googleFonts({
+    fonts:
+      "https://fonts.google.com/share?selection.family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900",
+  }));
+
+export default site;
